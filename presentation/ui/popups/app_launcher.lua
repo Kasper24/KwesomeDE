fc023ec28ee08f4115cbd0522f419c96f7375867
Ksaper <ofek4430@gmail.com>
2022-05-13 04:32:56 +0300

    Text only launcher

--- presentation/ui/popups/app_launcher.lua
+++ presentation/ui/popups/app_launcher.lua
@@ -1,3 +1,4 @@
+local awful = require("awful")
 local beautiful = require("beautiful")
 local bling = require("modules.bling")
 local helpers = require("helpers")
@@ -8,6 +9,15 @@ local instance = nil
 local function new()
     return bling.widget.app_launcher
     {
+        placement = function(widget)
+            awful.placement.top_left(widget,
+            {
+                honor_workarea = true,
+                honor_padding = true,
+                attach = true
+            })
+        end,
+        app_show_icon = false,
         skip_names = { "avahi", "Hardware Local", "networkmanager_dmenu" },
         type = "menu",
         terminal = "kitty",
@@ -16,21 +26,22 @@ local function new()
         prompt_margins = dpi(25),
         prompt_paddings = dpi(15),
         prompt_shape = helpers.ui.rrect(beautiful.border_radius),
-        prompt_color = helpers.color.lighten(beautiful.colors.surface, 20),
+        prompt_color = beautiful.colors.background,
         prompt_icon_color = beautiful.colors.on_background,
         prompt_text_color = beautiful.colors.on_background,
         prompt_cursor_color = beautiful.colors.on_background,
         app_default_icon = beautiful.profile_icon,
-        app_width = dpi(150),
-        app_height = dpi(150),
+        app_width = dpi(400),
+        app_height = dpi(75),
+        app_name_halign = "left",
         apps_spacing = dpi(15),
-        apps_per_row =  5,
-        apps_per_column = 5,
+        apps_per_row =  9,
+        apps_per_column = 1,
         app_selected_color = beautiful.random_accent_color(),
-        app_normal_color = string.sub(beautiful.colors.background, 1, 7) .. "00",
+        app_normal_color = beautiful.colors.background,
         app_shape = helpers.ui.rrect(beautiful.bor1der_radius),
-        app_name_font = beautiful.font_name .. 12,
-        apps_margin = { left = dpi(40), right  = dpi(40), bottom = dpi(30) }
+        app_name_font = beautiful.font_name .. 14,
+        -- apps_margin = { left = dpi(40), right  = dpi(40), bottom = dpi(30) }
     }
 end
 
