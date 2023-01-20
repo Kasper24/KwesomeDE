c94b32dd463b93f82cfdcd15c51ace6d77f452a7
Ksaper <ofek4430@gmail.com>
2023-01-21 00:35:39 +0200

    This improves loading times (thanks Ryex again)

--- rc.lua
+++ rc.lua
@@ -6,18 +6,22 @@
 pcall(require, "luarocks.loader")
 
 local gtimer = require("gears.timer")
-local beautiful = require("beautiful")
-local helpers = require("helpers")
 local collectgarbage = collectgarbage
 
-collectgarbage("setpause", 100)
-collectgarbage("setstepmul", 400)
+collectgarbage("setpause", 110)
+collectgarbage("setstepmul", 1000)
+gtimer({
+	timeout = 5,
+	autostart = true,
+	call_now = true,
+	callback = function()
+		collectgarbage("collect")
+	end,
+})
 
+local beautiful = require("beautiful")
+local helpers = require("helpers")
 beautiful.init(helpers.filesystem.get_awesome_config_dir("presentation") .. "theme/theme.lua")
 
 require("config")
 require("presentation")
-
-gtimer { timeout = 5, autostart = true, call_now = true, callback = function()
-    collectgarbage("collect")
-end }
