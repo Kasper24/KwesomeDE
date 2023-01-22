d757336d4e0722dd0c3bdae929f2b555b004de5c
Ksaper <ofek4430@gmail.com>
2023-01-23 00:03:29 +0200

    Window swither v2

--- presentation/ui/popups/window_switcher.lua
+++ presentation/ui/popups/window_switcher.lua
@@ -18,91 +18,64 @@ local window_switcher  = { }
 local instance = nil
 
 local function focus_client(client)
-    capi.client.focus = client
-
-    capi.client.focus:raise()
-    capi.client.focus.minimized = false
-
-    if capi.client.focus:tags() and capi.client.focus:tags()[1] then
-        capi.client.focus:tags()[1]:view_only()
-    else
-        capi.client.focus:tags({awful.screen.focused().selected_tag})
-    end
-end
-
-local function cycle_clients(self, increase)
-    self._private.selected_index = self._private.selected_index + (increase and 1 or -1)
-
-    if self._private.selected_index > #capi.client.get() then
-        self._private.selected_index = 1
-    end
-    if self._private.selected_index < 1 then
-        self._private.selected_index = #capi.client.get()
+    if not client:isvisible() and client.first_tag then
+        client.first_tag:view_only()
     end
 
-    focus_client(capi.client.get()[self._private.selected_index])
+    client:emit_signal('request::activate')
+    client:raise()
+    -- client.minimized = false
 end
 
-local function get_client_content_as_imagebox(c)
-    local ss = awful.screenshot {
-        client = c,
-    }
-
-    ss:refresh()
-    local ib = ss.content_widget
-    ib.valign = "center"
-    ib.halign = "center"
-    ib.horizontal_fit_policy = "fit"
-    ib.vertical_fit_policy = "fit"
-    ib.resize = true
-
-    return ib
-end
-
-local function client_button(client)
+local function client_widget(self, client)
     local font_icon = beautiful.get_font_icon_for_app_name(client.class)
-
-    for _, tag in ipairs(client:tags()) do
-        if tag.selected then
-            client.window_switcher_thumbnail = get_client_content_as_imagebox(client)
-        end
-    end
+    local is_selected = client == self._private.clients[self._private.selected_index]
 
     return wibox.widget
     {
-        widget = wibox.container.background,
-        id = "bg_role",
-        forced_width = dpi(150),
-        forced_height = dpi(250),
+        widget = wibox.container.constraint,
+        mode = "max",
+        width = dpi(300),
+        height = dpi(150),
         {
-            layout = wibox.layout.flex.vertical,
+            widget = wibox.container.background,
+            shape = helpers.ui.rrect(beautiful.border_radius),
+            border_width = dpi(5),
+            border_color = (is_selected) and beautiful.random_accent_color() or beautiful.colors.surface,
             {
-                widget = wibox.container.margin,
-                margins = dpi(5),
-                client.window_switcher_thumbnail
-            },
-            {
-                layout = wibox.layout.fixed.horizontal,
-                spacing = dpi(5),
+                widget = wibox.layout.stack,
                 {
-                    widget = wibox.container.place,
-                    forced_width = dpi(40),
-                    valign = "center",
-                    {
-                        widget = widgets.text,
-                        color = beautiful.random_accent_color(),
-                        font = font_icon.font,
-                        text = font_icon.icon
-                    }
+                    widget = wibox.container.background,
+                    shape = helpers.ui.rrect(beautiful.border_radius),
+                    bg = beautiful.colors.background,
                 },
                 {
                     widget = wibox.container.margin,
-                    margins = dpi(10),
+                    margins = dpi(15),
                     {
-                        widget = wibox.widget.textbox,
-                        forced_width = dpi(200),
-                        valign = "center",
-                        text = client.name
+                        layout = wibox.layout.fixed.vertical,
+                        spacing = dpi(15),
+                        {
+                            layout = wibox.layout.fixed.horizontal,
+                            spacing = dpi(10),
+                            {
+                                widget = widgets.text,
+                                halign = "center",
+                                valign ="center",
+                                color = beautiful.random_accent_color(),
+                                font = font_icon.font,
+                                text = font_icon.icon
+                            },
+                            {
+                                widget = widgets.text,
+                                forced_height = dpi(30),
+                                halign = "center",
+                                valign = "center",
+                                size = 15,
+                                text = client.name
+                            }
+                        },
+                        widgets.client_thumbnail(client)
                     }
                 }
             }
@@ -110,83 +83,55 @@ local function client_button(client)
     }
 end
 
+local function clients_widget(self)
+    local clients_layout = wibox.widget
+    {
+        layout = wibox.layout.fixed.horizontal,
+        spacing = dpi(15)
+    }
+
+    for _, client in ipairs(self._private.clients) do
+        clients_layout:add(client_widget(self, client))
+    end
+
+    return wibox.widget
+    {
+        widget = wibox.container.background,
+        shape = helpers.ui.rrect(beautiful.border_radius),
+        bg = beautiful.colors.background .. "C8",
+        {
+            widget = wibox.container.margin,
+            margins = dpi(15),
+            clients_layout
+        }
+    }
+end
+
+function window_switcher:cycle_clients(increase)
+    self._private.selected_index = self._private.selected_index + (increase and 1 or -1)
+    if self._private.selected_index > #self._private.clients then
+        self._private.selected_index = 1
+    elseif self._private.selected_index < 1 then
+        self._private.selected_index = #self._private.clients
+    end
+
+    self._private.widget.widget = clients_widget(self)
+end
+
 function window_switcher:show()
     if #capi.client.get() == 0 then
         return
     end
 
-    -- Store client that is focused in a variable
-    self._private.window_switcher_first_client = capi.client.focus
+    self._private.clients = capi.client.get()
 
-    -- Stop recording focus history
-    awful.client.focus.history.disable_tracking()
-
-    -- Go to previously focused client (in the tag)
-    awful.client.focus.history.previous()
-
-    -- Start the keygrabber
-    self._private.window_switcher_grabber = awful.keygrabber.run(function(_, key, event)
-        if event == "release" then
-            -- Hide if the modifier was released
-            -- We try to match Super or Alt or Control since we do not know which keybind is
-            -- used to activate the window switcher (the keybind is set by the user in keys.lua)
-            if key:match("Super") or key:match("Alt") or key:match("Control") then
-                self:hide()
-            end
-            -- Do nothing
-            return
-        end
-
-        -- Run function attached to key, if it exists
-        if self._private.keyboard_keys[key] then
-            self._private.keyboard_keys[key]()
-        end
-    end)
-
-    local layout = wibox.layout.fixed.horizontal()
-
-    for _, client in ipairs(capi.client.get()) do
-        layout:add(client_button(client))
-    end
-
-    local widget = wibox.widget
-    {
-        widget = wibox.container.background,
-        shape = helpers.ui.rrect(beautiful.border_radius),
-        bg = beautiful.colors.background,
-        {
-            widget = wibox.container.margin,
-            margins = dpi(10),
-            layout
-        }
-    }
-
-    self._private.widget.widget = widget
+    self._private.widget.widget = clients_widget(self)
     self._private.widget.visible = true
 end
 
 function window_switcher:hide()
-    -- Add currently focused client to history
-    if capi.client.focus then
-        local window_switcher_last_client = capi.client.focus
-        awful.client.focus.history.add(window_switcher_last_client)
-        -- Raise client that was focused originally
-        -- Then raise last focused client
-        if
-            self._private.window_switcher_first_client and self._private.window_switcher_first_client.valid
-        then
-            self._private.window_switcher_first_client:raise()
-            window_switcher_last_client:raise()
-        end
-    end
+    focus_client(self._private.clients[self._private.selected_index])
 
-    -- Resume recording focus history
-    awful.client.focus.history.enable_tracking()
-
-    -- Stop the key grabber
-    awful.keygrabber.stop(self._private.window_switcher_grabber)
-
-    -- Hide the widget
     self._private.widget.visible = false
     self._private.widget.widget = nil
 
@@ -201,22 +146,13 @@ function window_switcher:toggle()
     end
 end
 
-local function new(args)
-    args = args or {}
-
+local function new()
     local ret = gobject{}
-    ret._private = {}
-
-    -- The client that was focused when the window_switcher was activated
-    ret._private.window_switcher_first_client = {}
-    -- The mouse grabber object
-    ret._private.window_switcher_grabber = nil
-
-    ret._private.selected_index = 1
-
-
     gtable.crush(ret, window_switcher)
-    gtable.crush(ret, args)
+
+    ret._private = {}
+    ret._private.selected_index = 1
+    ret._private.clients = {}
 
     ret._private.widget = awful.popup
     {
@@ -228,91 +164,14 @@ local function new(args)
         widget = wibox.container.background, -- A dummy widget to make awful.popup not scream
     }
 
-    ret._private.mouse_keys = {
-        awful.button {
-            modifiers = { "Any" },
-            button = 1,
-            on_press = function(c)
-                capi.client.focus = c
-            end,
-        },
-
-        awful.button {
-            modifiers = { "Any" },
-            button = 4,
-            on_press = function()
-                awful.client.focus.byidx(-1)
-            end,
-        },
-
-        awful.button {
-            modifiers = { "Any" },
-            button = 5,
-            on_press = function()
-                awful.client.focus.byidx(1)
-            end,
-        }
-    }
-
-    ret._private.keyboard_keys = {
-        ["Escape"] = function()
-            ret:hide()
-        end,
-
-        ["n"] = function()
-            if capi.client.focus then
-                capi.client.focus.minimized = true
-            end
-        end,
-        ["N"] = function()
-            if awful.client.restore() then
-                capi.client.focus = awful.client.restore()
-            end
-        end,
-        ['q'] = function()
-            if capi.client.focus then
-                capi.client.focus:kill()
-            end
-        end,
-
-        ["Tab"] = function()
-            cycle_clients(ret, true)
-
-            -- capi.client.focus = gtable.cycle_value(capi.client.get(), capi.client.focus, 1)
-            -- capi.client.focus:raise()
-            -- capi.client.focus.minimized = false
-            -- capi.client.focus:tags()[1]:view_only()
-
-            -- if capi.client.focus:tags() and capi.client.focus:tags()[1] then
-            --     capi.client.focus:tags()[1]:view_only()
-            -- else
-            --     capi.client.focus:tags({awful.screen.focused().selected_tag})
-            -- end
-        end,
-
-        ["Left"] = function()
-            awful.client.focus.byidx(1)
-        end,
-        ["Right"] = function()
-            awful.client.focus.byidx(-1)
-        end,
-
-        ['h'] = function()
-            awful.client.focus.byidx(1)
-        end,
-        ['l'] = function()
-            awful.client.focus.byidx(-1)
-        end,
-    }
-
     ret._private.widget:connect_signal("property::width", function()
-        if ret._private.widget.visible and get_num_clients() == 0 then
+        if ret._private.widget.visible and #capi.client.get() == 0 then
             ret:hide()
         end
     end)
 
     ret._private.widget:connect_signal("property::height", function()
-        if ret._private.widget.visible and get_num_clients() == 0 then
+        if ret._private.widget.visible and #capi.client.get() == 0 then
             ret:hide()
         end
     end)
