fc8f2b3dec22dfddc4822f9a18431ca96956438d
Ksaper <ofek4430@gmail.com>
2023-01-24 02:29:07 +0200

    Titlebar background color based on client color (thanks to nice)

--- presentation/ui/titlebar/init.lua
+++ presentation/ui/titlebar/init.lua
@@ -3,12 +3,59 @@
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
 
+local lgi = require("lgi")
+local Gdk = lgi.Gdk
 local awful = require("awful")
+local gsurface = require("gears.surface")
 local wibox = require("wibox")
 local widgets = require("presentation.ui.widgets")
 local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
+local floor = math.floor
+local pairs = pairs
 local capi = { client = client }
+
+local function get_dominant_color(client)
+    local color
+    -- gsurface(client.content):write_to_png(
+    --     "/home/mutex/nice/" .. client.class .. "_" .. client.instance .. ".png")
+    local pb
+    local bytes
+    local tally = {}
+    local content = gsurface(client.content)
+    local cgeo = client:geometry()
+    local x_offset = 2
+    local y_offset = 2
+    local x_lim = floor(cgeo.width / 2)
+    for x_pos = 0, x_lim, 2 do
+        for y_pos = 0, 8, 1 do
+            pb = Gdk.pixbuf_get_from_surface(
+                     content, x_offset + x_pos, y_offset + y_pos, 1, 1)
+            bytes = pb:get_pixels()
+            color = "#" ..
+                        bytes:gsub(
+                            ".",
+                            function(c)
+                        return ("%02x"):format(c:byte())
+                    end)
+            if not tally[color] then
+                tally[color] = 1
+            else
+                tally[color] = tally[color] + 1
+            end
+        end
+    end
+    local mode
+    local mode_c = 0
+    for kolor, kount in pairs(tally) do
+        if kount > mode_c then
+            mode_c = kount
+            mode = kolor
+        end
+    end
+    color = mode
+    return color
+end
 
 capi.client.connect_signal("request::titlebars", function(c)
     -- No clue why by minimizng only works if I do it via on_release?
@@ -16,6 +63,7 @@ capi.client.connect_signal("request::titlebars", function(c)
     {
         forced_width = dpi(40),
         forced_height = dpi(40),
+        normal_bg = get_dominant_color(c),
         text_normal_bg = beautiful.colors.cyan,
         size = 12,
         font = beautiful.icons.triangle.font,
@@ -29,6 +77,7 @@ capi.client.connect_signal("request::titlebars", function(c)
     {
         forced_width = dpi(40),
         forced_height = dpi(40),
+        normal_bg = get_dominant_color(c),
         text_normal_bg = beautiful.colors.green,
         size = 12,
         font = beautiful.icons.square.font,
@@ -43,6 +92,7 @@ capi.client.connect_signal("request::titlebars", function(c)
     {
         forced_width = dpi(40),
         forced_height = dpi(40),
+        normal_bg = get_dominant_color(c),
         text_normal_bg = beautiful.colors.error,
         size = 12,
         font = beautiful.icons.circle.font,
@@ -68,12 +118,12 @@ capi.client.connect_signal("request::titlebars", function(c)
     {
         position = "top",
         size = dpi(35),
-        bg_normal = beautiful.colors.background,
-        bg_focus = beautiful.colors.background,
-        bg_urgent = beautiful.colors.background,
-        fg_normal = beautiful.colors.on_background,
-        fg_focus = beautiful.colors.on_background,
-        fg_urgent = beautiful.colors.on_background,
+        bg_normal = get_dominant_color(c),
+        bg_focus = get_dominant_color(c),
+        bg_urgent = get_dominant_color(c),
+        fg_normal = get_dominant_color(c),
+        fg_focus = get_dominant_color(c),
+        fg_urgent = get_dominant_color(c),
         font = beautiful.font_name .. 12
     }) : setup
     {
