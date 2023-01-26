136cc1c511fceb452529cda0d21b3b4dc6fc411b
Ksaper <ofek4430@gmail.com>
2023-01-26 03:36:18 +0200

    Not used either

--- presentation/ui/widgets/button/init.lua
+++ presentation/ui/widgets/button/init.lua
@@ -6,6 +6,5 @@
 return
 {
 	elevated = require("presentation.ui.widgets.button.elevated"),
-	image = require("presentation.ui.widgets.button.image"),
 	text = require("presentation.ui.widgets.button.text")
 }
