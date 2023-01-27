94da5156500cb584b9da5e21eb5b131f519e3765
Ksaper <ofek4430@gmail.com>
2023-01-28 01:06:45 +0200

    Steal orlando ham animated icon

--- presentation/ui/wibar/init.lua
+++ presentation/ui/wibar/init.lua
@@ -27,33 +27,112 @@ local capi = { awesome = awesome, root = root, screen = screen, client = client 
 -- =============================================================================
 --  Start button
 -- =============================================================================
+local function get_draw(pos, color)
+	return function(_, _, cr, _, height)
+		cr:set_source_rgb(color.r / 255, color.g / 255, color.b / 255)
+		cr:set_line_width(0.1 * height)
+
+		--top, middle, bottom, left, right, radius, radius/2 pi*2
+		local t, m, b, l, r, ra, ra2, pi2
+		t = 0.3 * height
+		m = 0.5 * height
+		b = 0.7 * height
+		l = 0.25 * height
+		r = 0.75 * height
+		ra = 0.05 * height
+		ra2 = ra/2
+		pi2 = math.pi * 2
+
+		if pos <= 0.5 then
+
+			local tpos = t+(m-t)*pos
+			local bpos = b - (b - m) * pos
+
+			pos = pos * 2
+
+			cr:arc(l, tpos, ra, 0, pi2)
+			cr:arc(r, tpos, ra, 0, pi2)
+			cr:fill()
+
+			cr:arc(l, m, ra, 0, pi2)
+			cr:arc(r, m, ra, 0, pi2)
+			cr:fill()
+
+			cr:arc(l, bpos, ra, 0, pi2)
+			cr:arc(r, bpos, ra, 0, pi2)
+			cr:fill()
+
+			cr:move_to(l + ra2, tpos)
+			cr:line_to(r - ra2, tpos)
+
+			cr:move_to(l + ra2, m)
+			cr:line_to(r - ra2, m)
+
+			cr:move_to(l + ra2, bpos)
+			cr:line_to(r - ra2, bpos)
+
+			cr:stroke()
+		else
+			pos = (pos - 0.5) * 2
+
+			cr:move_to(l, m - (m - l) * pos)
+			cr:line_to(r, m + (r - m) * pos)
+
+			cr:move_to(l, m + (r - m) * pos)
+			cr:line_to(r, m - (m - l) * pos)
+
+			cr:stroke()
+		end
+	end
+end
+
 local function start()
+    local on_color = helpers.color.hex_to_rgb(beautiful.random_accent_color())
+    local off_color = helpers.color.hex_to_rgb(beautiful.colors.on_background)
+
     local widget = wibox.widget
     {
-        widget = wibox.container.margin,
-        margins = dpi(5),
-        {
-            widget = widgets.button.text.state,
-            id = "button",
-            forced_width = dpi(60),
-            forced_height = dpi(60),
-            icon = beautiful.icons.bars_staggered,
-            size = 25,
-            on_release = function()
-                app_launcher:toggle()
-            end
-        }
+
+        forced_width = dpi(60),
+		forced_height = dpi(60),
+		draw = get_draw(0, off_color),
+		fit = function(_, _, _, height) return height, height end,
+		widget = wibox.widget.make_base_widget
+	}
+
+    local button = wibox.widget
+    {
+        widget = widgets.button.elevated.state,
+        on_release = function()
+            app_launcher:toggle()
+        end,
+        child = widget
     }
+
+	local animation = helpers.animation:new
+    {
+        pos =
+        {
+            height = 0,
+            color = off_color
+        },
+        easing = helpers.animation.easing.linear,
+        duration = 0.2,
+		update = function(self, pos)
+			widget.draw = get_draw(pos.height, pos.color)
+			widget:emit_signal("widget::redraw_needed")
+		end
+	}
 
     app_launcher:connect_signal("bling::app_launcher::visibility", function(self, visibility)
         if visibility == true then
-            widget:get_children_by_id("button")[1]:turn_on()
+            animation:set{height = 1, color = on_color}
         else
-            widget:get_children_by_id("button")[1]:turn_off()
+            animation:set{height = 0, color = off_color}
         end
     end)
 
-    return widget
+    return button
 end
 
 -- =============================================================================
