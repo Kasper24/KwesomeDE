573b1c7f57baa7438dc0ec66aaf87ed53d6aeff8
Ksaper <ofek4430@gmail.com>
2023-01-28 19:07:46 +0200

    Fix startup

--- config/startup.lua
+++ config/startup.lua
@@ -13,7 +13,6 @@ local function setup_system_tools()
    helpers.run.run_once_grep("parcellite")
    helpers.run.run_once_grep("kdeconnect-indicator")
    helpers.run.run_once_grep("mopidy")
-   helpers.run.run_once_grep("openrgb")
 end
 
 local function configure_keyboard()
@@ -23,17 +22,16 @@ local function configure_keyboard()
       awful.spawn("setxkbmap -layout us,il -variant , -option grp:alt_shift_toggle", false)
    end)
 
-   -- Why does xkbcomp only disable caps lock on release instead of on press like any other key?
    awful.spawn.with_shell
    ([[
-         xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
-            repeat=no,\
-            type[group1]="ALPHABETIC",\
-            symbols[group1]=[ Caps_Lock, Caps_Lock],\
-            actions[group1]=[ LockMods(modifiers=Lock),\
-            Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
-         };\
-         #' | xkbcomp -w 0 - "$DISPLAY"
+xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
+   repeat=no,\
+   type[group1]="ALPHABETIC",\
+   symbols[group1]=[ Caps_Lock, Caps_Lock],\
+   actions[group1]=[ LockMods(modifiers=Lock),\
+   Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
+};\
+#' | xkbcomp -w 0 - "$DISPLAY"
    ]])
 end
 
