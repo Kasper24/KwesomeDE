bd81e9835389a05e899ae3283264fdd118a7ef08
Ksaper <ofek4430@gmail.com>
2023-02-04 23:01:18 +0200

    Won't be visible anyway until the next restart

--- ui/popups/loading.lua
+++ ui/popups/loading.lua
@@ -97,9 +97,4 @@ capi.screen.connect_signal("request::desktop_decoration", function(s)
             end
         }
     end
-
-
-    capi.awesome.connect_signal("colorscheme::changed", function(old_colorscheme_to_new_map)
-        s.loading_popup.bg = old_colorscheme_to_new_map[beautiful.colors.background]
-    end)
 end)
