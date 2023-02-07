1d842174405c4846357f23e2de7129cd4780f9f8
Ksaper <ofek4430@gmail.com>
2023-02-07 15:56:29 +0200

    Fix default layout icons

--- ui/theme.lua
+++ ui/theme.lua
@@ -2,6 +2,7 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
+local gfs = require("gears.filesystem")
 local gcolor = require("gears.color")
 local beautiful = require("beautiful")
 local theme_daemon = require("daemons.system.theme")
@@ -73,6 +74,23 @@ local function colors()
 end
 
 local function icons()
+    local themes_path = gfs.get_themes_dir()
+    theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+    theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+    theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+    theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+    theme.layout_max = themes_path.."default/layouts/maxw.png"
+    theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+    theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+    theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+    theme.layout_tile = themes_path.."default/layouts/tilew.png"
+    theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+    theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+    theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+    theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+    theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+    theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+    theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
     beautiful.theme_assets.recolor_layout(theme, theme.colors.on_background)
 
     local font_awesome_6_solid_font_name = "Font Awesome 6 Pro Solid "
