dc0a5f206c5b45e1f2a5fe51dcc8d2e7a6afae38
Ksaper <ofek4430@gmail.com>
2023-02-09 17:35:38 +0200

    Make client thumbnail refresh it's content if needed

--- ui/widgets/client_thumbnail.lua
+++ ui/widgets/client_thumbnail.lua
@@ -3,6 +3,7 @@
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
 local awful = require("awful")
+local gtimer = require("gears.timer")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local cfiwidget = require("ui.widgets.client_font_icon")
@@ -28,23 +29,28 @@ local function get_client_thumbnail(client)
     return client.thumbnail
 end
 
+local function add_titlebar(client, preview)
+    client.titlebar_preview = wibox.widget {
+        widget = wibox.widget.imagebox,
+        horizontal_fit_policy = "fit",
+        vertical_fit_policy = "fit",
+        image = wibox.widget.draw_to_image_surface(client.titlebar_widget.widget, client.width, client.titlebar_size)
+    }
+
+    preview:insert(1, client.titlebar_preview)
+end
+
 local function new(client)
-    local thumbnail = get_client_thumbnail(client)
-    local widget = thumbnail and wibox.widget {
+    local preview_image = wibox.widget {
+        widget = wibox.widget.imagebox,
+    }
+
+    local preview = wibox.widget {
         layout = wibox.layout.fixed.vertical,
-        client.titlebar and {
-            widget = wibox.widget.imagebox,
-            horizontal_fit_policy = "fit",
-            vertical_fit_policy = "fit",
-            image = wibox.widget.draw_to_image_surface(client.titlebar.widget, client.width, client.titlebar_size)
-        } or nil,
-        {
-            widget = wibox.widget.imagebox,
-            horizontal_fit_policy = "fit",
-            vertical_fit_policy = "fit",
-            image = thumbnail
-        }
-    } or wibox.widget {
+        preview_image
+    }
+
+    local fake_preview = wibox.widget {
         widget = cfiwidget,
         forced_width = dpi(300),
         forced_height = dpi(300),
@@ -53,6 +59,41 @@ local function new(client)
         size = client.font_icon.size * 2,
         client = client
     }
+
+    local widget = fake_preview
+
+    if client.titlebar_enabled then
+        add_titlebar(client, preview)
+    end
+
+    client:connect_signal("property::titlebar_enabled", function(client)
+        if client.titlebar_enabled then
+            add_titlebar(client, preview)
+        else
+            preview:remove_widgets(client.titlebar_preview)
+        end
+    end)
+
+    client:connect_signal("focus", function(client)
+        if preview_image.image == nil then
+            gtimer {
+                timeout = 0.2,
+                single_shot = true,
+                autostart = true,
+                call_now = false,
+                callback = function()
+                    preview_image.image = get_client_thumbnail(client)
+                    widget = preview
+                end
+            }
+        end
+    end)
+
+    local thumbnail = get_client_thumbnail(client)
+    if thumbnail then
+        preview_image.image = get_client_thumbnail(client)
+        widget = preview
+    end
 
     return widget
 end
--- ui/widgets/titlebar.lua
+++ ui/widgets/titlebar.lua
@@ -12,10 +12,60 @@ local titlebar = {
 local function new(c, args)
     local widget = awful.titlebar(c, args)
 
-    c.titlebar = widget
+    c.titlebar_widget = widget
     c.titlebar_size = args.size
 
     return widget
+end
+
+local function get_titlebar_function(c, position)
+    if position == "left" then
+        return c.titlebar_left
+    elseif position == "right" then
+        return c.titlebar_right
+    elseif position == "top" then
+        return c.titlebar_top
+    elseif position == "bottom" then
+        return c.titlebar_bottom
+    else
+        error("Invalid titlebar position '" .. position .. "'")
+    end
+end
+
+local function load_titlebars(c, hide_all, keep, context)
+    if c._request_titlebars_called then return false end
+
+    c:emit_signal("request::titlebars", context, {})
+
+    if hide_all then
+        -- Don't bother checking if it has been created, `.hide` don't works
+        -- anyway.
+        for _, tb in ipairs {"top", "bottom", "left", "right"} do
+            if tb ~= keep then
+                awful.titlebar.hide(c, tb)
+            end
+        end
+    end
+
+    c._request_titlebars_called = true
+
+    return true
+end
+
+function awful.titlebar.toggle(c, position)
+    position = position or "top"
+    if load_titlebars(c, true, position, "toggle") then
+        c.titlebar_enabled = true
+        return
+    end
+    local _, size = get_titlebar_function(c, position)(c)
+    if size == 0 then
+        awful.titlebar.show(c, position)
+        c.titlebar_enabled = true
+    else
+        awful.titlebar.hide(c, position)
+        c.titlebar_enabled = false
+    end
 end
 
 function titlebar.mt:__call(...)
