5f6aa96d9a3c8de2eda9ee8b93bdac2308f18beb
Ksaper <ofek4430@gmail.com>
2023-02-14 03:11:41 +0200

    Change layout ordering

--- config/layouts.lua
+++ config/layouts.lua
@@ -10,24 +10,30 @@ local capi = {
 }
 
 capi.tag.connect_signal("request::default_layouts", function()
-    awful.layout.append_default_layouts({awful.layout.suit.tile.right, -- The main tile algo, on the right.
-    -- awful.layout.suit.tile.left, -- The main tile algo, on the left.
-    -- awful.layout.suit.tile.bottom, -- The main tile algo, on the bottom.
-    -- awful.layout.suit.tile.top, -- The main tile algo, on the top.
-    -- awful.layout.suit.corner.nw, --Corner layout.
-    -- awful.layout.suit.corner.ne, -- Corner layout.
-    -- awful.layout.suit.corner.sw,	-- Corner layout.
-    -- awful.layout.suit.corner.se, -- Corner layout.
-    -- awful.layout.suit.fair, -- The fair layout.
-    -- awful.layout.suit.fair.horizontal, -- The horizontal fair layout.
-    awful.layout.suit.floating, -- The floating layout.
-    -- awful.layout.suit.magnifier, -- The magnifier layout.
-    -- awful.layout.suit.max, -- Maximized layout.
-    -- awful.layout.suit.max.fullscreen, -- Fullscreen layout.
-    -- awful.layout.suit.spiral.dwindle, -- Dwindle layout.
-    -- bling.layout.mstab,
-    bling.layout.centered, bling.layout.vertical, bling.layout.horizontal, bling.layout.equalarea, bling.layout.deck,
-                                         machi.default_layout})
+    awful.layout.append_default_layouts{
+        awful.layout.suit.tile.right,
+        -- awful.layout.suit.tile.left,
+        -- awful.layout.suit.tile.bottom,
+        -- awful.layout.suit.tile.top,
+        -- awful.layout.suit.corner.nw,.
+        -- awful.layout.suit.corner.ne,
+        -- awful.layout.suit.corner.sw,
+        -- awful.layout.suit.corner.se,
+        -- awful.layout.suit.fair,
+        -- awful.layout.suit.fair.horizontal,
+        -- awful.layout.suit.magnifier,
+        -- awful.layout.suit.max,
+        -- awful.layout.suit.max.fullscreen,
+        -- awful.layout.suit.spiral.dwindle,
+        -- bling.layout.mstab,
+        bling.layout.centered,
+        bling.layout.vertical,
+        bling.layout.horizontal,
+        bling.layout.equalarea,
+        bling.layout.deck,
+        awful.layout.suit.floating,
+        machi.default_layout
+    }
 end)
 
 machi.editor.nested_layouts["4"] = bling.layout.deck
