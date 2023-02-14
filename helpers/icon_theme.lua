0d94dd2a084983c7945ff58d436a2aa59e0b8166
Ksaper <ofek4430@gmail.com>
2023-02-14 05:07:47 +0200

    this works

--- helpers/icon_theme.lua
+++ helpers/icon_theme.lua
@@ -5,27 +5,15 @@
 local lgi = require("lgi")
 local Gio = lgi.Gio
 local DesktopAppInfo = Gio.DesktopAppInfo
+local AppInfo = Gio.DesktopAppInfo
 local Gtk = lgi.require("Gtk", "3.0")
+local string = string
+local ipairs = ipairs
 
 local ICON_SIZE = 48
 local GTK_THEME = Gtk.IconTheme.get_default()
 
 local _icon_theme = {}
-
-function _icon_theme.get_client_icon_path(client, icon_theme, icon_size)
-    local desktop_app_info_filename = DesktopAppInfo.search(client.class)[1][1]
-    if desktop_app_info_filename then
-        local desktop_app_info = DesktopAppInfo.new(desktop_app_info_filename)
-        if desktop_app_info then
-            local icon_name = desktop_app_info:get_string("Icon")
-            if icon_name then
-                return _icon_theme.get_icon_path(icon_name, icon_theme, icon_size)
-            end
-        end
-    end
-
-    _icon_theme.choose_icon({"window", "window-manager", "xfwm4-default", "window_list"}, icon_theme, icon_size)
-end
 
 function _icon_theme.choose_icon(icons_names, icon_theme, icon_size)
     if icon_theme then
@@ -89,6 +77,37 @@ function _icon_theme.get_icon_path(icon_name, icon_theme, icon_size)
     end
 
     return ""
+end
+
+function _icon_theme.get_client_icon_path(client, icon_theme, icon_size)
+    local app_list = AppInfo.get_all()
+
+    local class = string.lower(client.class)
+    local name = string.lower(client.name)
+
+    for _, app in ipairs(app_list) do
+        local id = app:get_id()
+        local desktop_app_info = DesktopAppInfo.new(id)
+        if desktop_app_info then
+            local props = {
+                string.lower(desktop_app_info:get_string("Name") or ""),
+                string.lower(desktop_app_info:get_filename() or ""),
+                string.lower(desktop_app_info:get_startup_wm_class() or ""),
+                string.lower(desktop_app_info:get_string("Icon") or ""),
+                string.lower(desktop_app_info:get_string("Exec") or ""),
+                string.lower(desktop_app_info:get_string("Keywords") or "")
+            }
+
+            for _, prop in ipairs(props) do
+                if prop ~= "" and (prop == class or prop == name) then
+                    local icon = desktop_app_info:get_string("Icon")
+                    return _icon_theme.get_icon_path(icon, icon_theme, icon_size)
+                end
+            end
+        end
+    end
+
+    return _icon_theme.choose_icon({"window", "window-manager", "xfwm4-default", "window_list"}, icon_theme, icon_size)
 end
 
 return _icon_theme
