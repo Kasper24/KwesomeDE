4efb6034b499a3117215ee793ede1774108a57d1
Ksaper <ofek4430@gmail.com>
2023-02-15 17:04:41 +0200

    Improve github and gitlab notifs

--- ui/notifications/github.lua
+++ ui/notifications/github.lua
@@ -2,6 +2,7 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
+local awful = require("awful")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local github_daemon = require("daemons.web.github")
@@ -9,47 +10,90 @@ local github_daemon = require("daemons.web.github")
 local icons = {"github", "appimagekit-github-desktop", "io.github.shiftey.Desktop", "folder-github", "folder-Github"}
 
 local function generate_action_string(event)
+    local action_string = event.type
+    local icon = "repo.svg"
+    local link = "http://github.com/" .. event.repo.name
+
     if (event.type == "PullRequestEvent") then
-        return event.payload.action .. " a pull request in"
+        action_string = event.payload.action .. " a pull request in"
+        link = event.payload.pull_request.html_url
+        icon = beautiful.icons.code_pull_request
     elseif (event.type == "PullRequestReviewCommentEvent") then
-        return event.payload.action == "created" and "commented in pull request" or event.payload.action ..
-                   " a comment in"
+        action_string = event.payload.action == "created" and "commented in pull request" or event.payload.action ..
+                            " a comment in"
+        link = event.payload.pull_request.html_url
+        icon = beautiful.icons.message
     elseif (event.type == "IssuesEvent") then
-        return event.payload.action .. " an issue in"
+        action_string = event.payload.action .. " an issue in"
+        link = event.payload.issue.html_url
+        icon = beautiful.icons.circle_exclamation
     elseif (event.type == "IssueCommentEvent") then
-        return event.payload.action == "created" and "commented in issue" or event.payload.action .. " a comment in"
+        action_string = event.payload.action == "created" and "commented in issue" or event.payload.action ..
+                            " a comment in"
+        link = event.payload.issue.html_url
+        icon = beautiful.icons.message
     elseif (event.type == "WatchEvent") then
-        return "starred"
+        action_string = "starred"
+        icon = beautiful.icons.star
     elseif (event.type == "PushEvent") then
-        return "pushed to"
+        action_string = "pushed to"
+        icon = beautiful.icons.commit
     elseif (event.type == "ForkEvent") then
-        return "forked"
+        action_string = "forked"
+        icon = beautiful.icons.code_branch
     elseif (event.type == "CreateEvent") then
-        return "created"
+        action_string = "created"
+        icon = beautiful.icons.code_branch
     end
+
+    return {
+        action_string = action_string,
+        link = link,
+        icon = icon
+    }
 end
 
 github_daemon:connect_signal("new_event", function(self, event, avatar_path)
+    local action_and_link = generate_action_string(event)
+
+    local open = naughty.action {
+        name = "Open"
+    }
+
+    open:connect_signal("invoked", function()
+        awful.spawn("xdg-open " .. action_and_link.link, false)
+    end)
+
     naughty.notification {
         app_font_icon = beautiful.icons.github,
         app_icon = icons,
         app_name = "Github",
         icon = avatar_path,
-        title = "New Event",
-        text = event.actor.display_login .. " " .. generate_action_string(event) .. " " .. event.repo.name,
-        category = "email.arrived"
+        title = action_and_link.icon,
+        text = event.actor.display_login .. " " .. action_and_link.action_string .. " " .. event.repo.name,
+        category = "email.arrived",
+        actions = { open }
     }
 end)
 
 github_daemon:connect_signal("new_pr", function(self, pr)
+    local open = naughty.action {
+        name = "Open"
+    }
+
+    open:connect_signal("invoked", function()
+        awful.spawn("xdg-open " .. pr.html_url, false)
+    end)
+
     naughty.notification {
         app_font_icon = beautiful.icons.github,
         app_icon = icons,
         app_name = "Github",
         font_icon = beautiful.icons.envelope,
         icon = icons,
-        title = "New PR",
-        text = pr.title,
-        category = "email.arrived"
+        title = pr.title,
+        text = pr.body,
+        category = "email.arrived",
+        actions = { open }
     }
 end)
--- ui/notifications/gitlab.lua
+++ ui/notifications/gitlab.lua
@@ -2,6 +2,7 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
+local awful = require("awful")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 local gitlab_daemon = require("daemons.web.gitlab")
@@ -9,15 +10,24 @@ local gitlab_daemon = require("daemons.web.gitlab")
 local icons = {"gitlab", "com.github.zren.gitlabissues", "gitlab-tray", "folder-gitlab", "io.gitlab.osslugaru.Lugaru",
                "com.gitlab.davem.ClamTk", "mailer", "preferences-mail.svg", "kmail"}
 
-gitlab_daemon:connect_signal("new_pr", function(self, pr)
+gitlab_daemon:connect_signal("new_mr", function(self, mr)
+    local open = naughty.action {
+        name = "Open"
+    }
+
+    open:connect_signal("invoked", function()
+        awful.spawn("xdg-open " .. mr.web_url, false)
+    end)
+
     naughty.notification {
         app_font_icon = beautiful.icons.gitlab,
         app_icon = icons,
         app_name = "Gitlab",
         font_icon = beautiful.icons.envelope,
         icon = icons,
-        title = "New PR",
-        text = pr.title,
-        category = "email.arrived"
+        title = mr.title,
+        text = mr.description,
+        category = "email.arrived",
+        actions = { open }
     }
 end)
