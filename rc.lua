c30bccba6d44b332a61d4717e290a499b7bf57dc
Ksaper <ofek4430@gmail.com>
2023-02-19 08:26:45 +0200

    Formatting

--- rc.lua
+++ rc.lua
@@ -20,12 +20,11 @@ gtimer{
 }
 
 local beautiful = require("beautiful")
-local filesystem = require("external.filesystem")
-beautiful.init(filesystem.filesystem.get_awesome_config_dir("ui") .. "theme.lua")
-
 local theme_daemon = require("daemons.system.theme")
-local beautiful = require("beautiful")
+local filesystem = require("external.filesystem")
+
 beautiful.xresources.set_dpi(theme_daemon:get_dpi())
+beautiful.init(filesystem.filesystem.get_awesome_config_dir("ui") .. "theme.lua")
 
 require("config")
 require("ui")
