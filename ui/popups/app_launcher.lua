d1e31b7f3bbb47713249db226a58e81ebfc3beac
Ksaper <ofek4430@gmail.com>
2023-02-25 00:17:24 +0200

    Extract the app function

--- ui/popups/app_launcher.lua
+++ ui/popups/app_launcher.lua
@@ -15,6 +15,100 @@ local capi = {
 
 local instance = nil
 
+local function app(app)
+    local font_icon = tasklist_daemon:get_font_icon(app.id:gsub(".desktop", ""),
+        app.name,
+        app.exec,
+        app.startup_wm_class,
+        app.icon_name
+    )
+
+    local menu = widgets.menu {
+        widgets.menu.button {
+            icon = font_icon,
+            text = app.name,
+            on_press = function(self)
+                app:spawn()
+            end
+        },
+        widgets.menu.checkbox_button {
+            state = tasklist_daemon:is_app_pinned{id = app.id},
+            handle_active_color = font_icon.color,
+            text = "Pin App",
+            on_press = function(self)
+                if tasklist_daemon:is_app_pinned{id = app.id} then
+                    self:turn_off()
+                    tasklist_daemon:remove_pinned_app{id = app.id}
+                else
+                    self:turn_on()
+                    tasklist_daemon:add_pinned_app{id = app.id}
+                end
+            end
+        }
+    }
+
+    for index, action in ipairs(app.desktop_app_info:list_actions()) do
+        if index == 1 then
+            menu:add(widgets.menu.separator())
+        end
+
+        menu:add(widgets.menu.button {
+            text = app.desktop_app_info:get_action_name(action),
+            on_press = function()
+                app.desktop_app_info:launch_action(action)
+            end
+        })
+    end
+
+    local widget = wibox.widget {
+        widget = widgets.button.elevated.state,
+        id = "button",
+        forced_width = dpi(120),
+        forced_height = dpi(120),
+        paddings = dpi(15),
+        halign = "center",
+        text = app.name,
+        on_secondary_press = function()
+            menu:toggle()
+        end,
+        child = {
+            layout = wibox.layout.fixed.vertical,
+            spacing = dpi(15),
+            {
+                widget = wibox.container.place,
+                halign = "center",
+                valign = "center",
+                {
+                    widget = widgets.text,
+                    size = 40,
+                    icon = font_icon
+                },
+            },
+            {
+                widget = wibox.container.place,
+                halign = "center",
+                valign = "center",
+                {
+                    widget = widgets.text,
+                    size = 12,
+                    color = beautiful.colors.on_background,
+                    text = app.name
+                }
+            }
+        }
+    }
+
+    widget:connect_signal("selected", function()
+        widget:turn_on()
+    end)
+
+    widget:connect_signal("unselected", function()
+        widget:turn_off()
+    end)
+
+    return widget
+end
+
 local function new()
     local app_launcher = bling.widget.app_launcher {
         bg = beautiful.colors.background,
@@ -68,98 +162,7 @@ local function new()
                 }
             },
         },
-        app_template = function(app)
-            local font_icon = tasklist_daemon:get_font_icon(app.id:gsub(".desktop", ""),
-                app.name,
-                app.startup_wm_class,
-                app.icon_name
-            )
-
-            local menu = widgets.menu {
-                widgets.menu.button {
-                    icon = font_icon,
-                    text = app.name,
-                    on_press = function(self)
-                        app:spawn()
-                    end
-                },
-                widgets.menu.checkbox_button {
-                    state = tasklist_daemon:is_app_pinned{id = app.id},
-                    handle_active_color = font_icon.color,
-                    text = "Pin App",
-                    on_press = function(self)
-                        if tasklist_daemon:is_app_pinned{id = app.id} then
-                            self:turn_off()
-                            tasklist_daemon:remove_pinned_app{id = app.id}
-                        else
-                            self:turn_on()
-                            tasklist_daemon:add_pinned_app{id = app.id}
-                        end
-                    end
-                }
-            }
-
-            for index, action in ipairs(app.desktop_app_info:list_actions()) do
-                if index == 1 then
-                    menu:add(widgets.menu.separator())
-                end
-
-                menu:add(widgets.menu.button {
-                    text = app.desktop_app_info:get_action_name(action),
-                    on_press = function()
-                        app.desktop_app_info:launch_action(action)
-                    end
-                })
-            end
-
-            local widget = wibox.widget {
-                widget = widgets.button.elevated.state,
-                id = "button",
-                forced_width = dpi(120),
-                forced_height = dpi(120),
-                paddings = dpi(15),
-                halign = "center",
-                text = app.name,
-                on_secondary_press = function()
-                    menu:toggle()
-                end,
-                child = {
-                    layout = wibox.layout.fixed.vertical,
-                    spacing = dpi(15),
-                    {
-                        widget = wibox.container.place,
-                        halign = "center",
-                        valign = "center",
-                        {
-                            widget = widgets.text,
-                            size = 40,
-                            icon = font_icon
-                        },
-                    },
-                    {
-                        widget = wibox.container.place,
-                        halign = "center",
-                        valign = "center",
-                        {
-                            widget = widgets.text,
-                            size = 12,
-                            color = beautiful.colors.on_background,
-                            text = app.name
-                        }
-                    }
-                }
-            }
-
-            widget:connect_signal("selected", function()
-                widget:turn_on()
-            end)
-
-            widget:connect_signal("unselected", function()
-                widget:turn_off()
-            end)
-
-            return widget
-        end
+        app_template = app
     }
 
     local animation = helpers.animation:new{
