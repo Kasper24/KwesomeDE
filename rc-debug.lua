a5b7595f5e3d15907378daa490ec83cb8c05486a
Ksaper <ofek4430@gmail.com>
2023-03-02 12:47:29 +0200

    Didn't work correctly anyway

--- rc-debug.lua
+++ rc-debug.lua
@@ -3,18 +3,4 @@
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
 DEBUG = true
-
-local helpers = require("helpers")
-local filesystem = require("external.filesystem")
-local capi = {
-    awesome = awesome
-}
-
-local path = filesystem.filesystem.get_awesome_config_dir("")
-local watcher = helpers.inotify:watch(path, {helpers.inotify.Events.modify})
-
-watcher:connect_signal("event", function(_, __, __)
-    capi.awesome.restart()
-end)
-
 require("rc")
