14f48c6b5c61c6dd15c2a77b89977139dc0a77a3
Ksaper <ofek4430@gmail.com>
2023-03-03 03:37:18 +0200

    Add vscode debug config

--- rc-debug.lua
+++ rc-debug.lua
@@ -2,5 +2,8 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
+if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
+    require("lldebugger").start()
+end
 DEBUG = true
 require("rc")
