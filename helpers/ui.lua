bfd5aff7829c6c1e41256ee8f05a4318752882f8
Ksaper <ofek4430@gmail.com>
2023-03-03 03:29:22 +0200

    Add some functions to adjust images res

--- helpers/ui.lua
+++ helpers/ui.lua
@@ -1,7 +1,14 @@
+local lgi = require('lgi')
+local GdkPixbuf = lgi.GdkPixbuf
+local gsurface = require("gears.surface")
 local gshape = require("gears.shape")
 local gmatrix = require("gears.matrix")
 local beautiful = require("beautiful")
 local ipairs = ipairs
+local floor = math.floor
+local capi = {
+    awesome = awesome
+}
 
 local _ui = {}
 
@@ -43,6 +50,34 @@ end
 
 function _ui.get_widget_geometry(wibox, widget)
     return _get_widget_geometry(wibox._drawable._widget_hierarchy, widget)
+end
+
+function _ui.adjust_image_res(path, height, width)
+    -- Load the original image file
+    local pixbuf = GdkPixbuf.Pixbuf.new_from_file(path)
+
+    -- Scale down the image
+    local scaled_pixbuf = pixbuf:scale_simple(width, height, GdkPixbuf.InterpType.BILINEAR)
+
+    return capi.awesome.pixbuf_to_surface(scaled_pixbuf._native, path)
+end
+
+function _ui.adjust_image_res_by_ratio(path, ratio)
+    -- Load the original image file
+    local pixbuf = GdkPixbuf.Pixbuf.new_from_file(path)
+
+    -- Get the original image dimensions
+    local width = pixbuf:get_width()
+    local height = pixbuf:get_height()
+
+    -- Calculate the new dimensions to scale the image down to
+    local new_width = floor(width / ratio)
+    local new_height = floor(height / ratio)
+
+    -- Scale down the image
+    local scaled_pixbuf = pixbuf:scale_simple(new_width, new_height, GdkPixbuf.InterpType.BILINEAR)
+
+    return capi.awesome.pixbuf_to_surface(scaled_pixbuf._native, path)
 end
 
 return _ui
