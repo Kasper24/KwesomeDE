a0f3b1f0c048354c23ec15ab624b46e539db9548
Ksaper <ofek4430@gmail.com>
2023-03-03 21:24:57 +0200

    Just use start_new

--- rc.lua
+++ rc.lua
@@ -2,7 +2,6 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
-
 pcall(require, "luarocks.loader")
 
 local gtimer = require("gears.timer")
@@ -10,14 +9,9 @@ local collectgarbage = collectgarbage
 
 collectgarbage("setpause", 110)
 collectgarbage("setstepmul", 1000)
-gtimer{
-	timeout = 5,
-	autostart = true,
-	call_now = true,
-	callback = function()
-		collectgarbage("collect")
-	end,
-}
+gtimer.start_new(5, function()
+	collectgarbage("collect")
+end)
 
 local beautiful = require("beautiful")
 local theme_daemon = require("daemons.system.theme")
