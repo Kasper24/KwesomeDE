ca0a800f80d68b85d2762edd6ce73c62246ab90e
Ksaper <ofek4430@gmail.com>
2023-03-08 06:41:02 +0200

    Some small code improvements to the startup file

--- config/startup.lua
+++ config/startup.lua
@@ -6,7 +6,7 @@ local awful = require("awful")
 local gtimer = require("gears.timer")
 local helpers = require("helpers")
 
-local function setup_system_tools()
+local function run_programs()
     helpers.run.run_once_ps("polkit-gnome-authentication-agent-1",
         "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
     helpers.run.run_once_grep("blueman-applet")
@@ -20,26 +20,33 @@ local function setup_system_tools()
 end
 
 local function configure_keyboard()
-    awful.spawn("xset r rate 200 30", false)
+    gtimer {
+        autostart = true,
+        call_now = true,
+        single_shot = false,
+        timeout = 600,
+        callback = function()
+            awful.spawn("xset r rate 200 30", false)
 
-    gtimer.delayed_call(function()
-        awful.spawn("setxkbmap -layout us,il -variant , -option grp:alt_shift_toggle", false)
-    end)
+            gtimer.delayed_call(function()
+                awful.spawn("setxkbmap -layout us,il -variant , -option grp:alt_shift_toggle", false)
+            end)
 
-    awful.spawn.with_shell([[
-xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
-   repeat=no,\
-   type[group1]="ALPHABETIC",\
-   symbols[group1]=[ Caps_Lock, Caps_Lock],\
-   actions[group1]=[ LockMods(modifiers=Lock),\
-   Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
-};\
-#' | xkbcomp -w 0 - "$DISPLAY"
-   ]])
+            awful.spawn.with_shell([[
+        xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
+           repeat=no,\
+           type[group1]="ALPHABETIC",\
+           symbols[group1]=[ Caps_Lock, Caps_Lock],\
+           actions[group1]=[ LockMods(modifiers=Lock),\
+           Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
+        };\
+        #' | xkbcomp -w 0 - "$DISPLAY"
+           ]])
+        end
+    }
 end
 
-local function configure_xserver()
-    -- awful.spawn("xrandr --output DP-2 --gamma 0.8", false)
+local function configure_display()
     awful.spawn("xset s off", false)
     awful.spawn("xset -dpms", false)
     awful.spawn("xset s noblank", false)
@@ -52,5 +59,6 @@ local function configure_xserver()
     }
 end
 
-setup_system_tools()
-configure_xserver()
+run_programs()
+configure_keyboard()
+configure_display()
