e3c60bb53ff70710973d59dc6dc7ef4395ee3521
Ksaper <ofek4430@gmail.com>
2023-03-16 02:56:22 +0200

    Rename updates_available to updates

--- ui/notifications/package_manager.lua
+++ ui/notifications/package_manager.lua
@@ -6,7 +6,7 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local system_daemon = require("daemons.system.system")
 
-system_daemon:connect_signal("package_manager::updates_available", function(self, name, updates_count, updates)
+system_daemon:connect_signal("package_manager::updates", function(self, name, updates_count, updates)
     local title = updates_count .. " Updates availabvle"
     if updates_count == 0 then
         title = "Your system is up to date"
