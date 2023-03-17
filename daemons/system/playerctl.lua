41a377dd711dff3b8c8a4ac78cb6c957181f8bd4
Ksaper <ofek4430@gmail.com>
2023-03-17 09:10:56 +0200

    If playerctl lib is not available, try to use playerctl cli

--- daemons/system/playerctl.lua
+++ daemons/system/playerctl.lua
@@ -2,16 +2,33 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
+local gdebug = require("gears.debug")
 local bling = require("external.bling")
 
 local instance = nil
 
 local function new()
-    return bling.signal.playerctl.lib {
-        update_on_activity = true,
-        player = {"spotify", "%any", "mopidy"},
-        debounce_delay = 1
-    }
+    local _playerctl_status, Playerctl = pcall(function()
+        return require("lgi").Playerctl
+    end)
+    if not _playerctl_status or not Playerctl then
+        gdebug.print_warning(
+            "Can't load Playerctl introspection. "..
+            "Seems like Playerctl is not installed or `lua-lgi` was built with an incompatible Playerctl version. " ..
+            "Using playerctl cli instead!"
+        )
+        return bling.signal.playerctl.cli {
+            update_on_activity = true,
+            player = {"spotify", "%any", "mopidy"},
+            debounce_delay = 1
+        }
+    else
+        return bling.signal.playerctl.lib {
+            update_on_activity = true,
+            player = {"spotify", "%any", "mopidy"},
+            debounce_delay = 1
+        }
+    end
 end
 
 if not instance then
