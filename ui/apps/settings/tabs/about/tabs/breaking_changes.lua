ce7755c13d05564807988868995e25d0ddec6f2f
Ksaper <ofek4430@gmail.com>
2023-03-30 23:16:30 +0300

    Improve breaking changes ui

--- ui/apps/settings/tabs/about/tabs/breaking_changes.lua
+++ ui/apps/settings/tabs/about/tabs/breaking_changes.lua
@@ -1,3 +1,4 @@
+local gshape = require("gears.shape")
 local wibox = require("wibox")
 local widgets = require("ui.widgets")
 local beautiful = require("beautiful")
@@ -10,20 +11,33 @@ local breaking_changes = {
     mt = {}
 }
 
-local function new()
-    local gnome_keyring = wibox.widget {
-        widget = widgets.text,
-        size = 15,
-        text = "Secrets are now stored in a secured way. Please install gnome-keyring and re-set your Gitlab API key and Openweather access token"
+local function bullet_point(message)
+    return wibox.widget {
+        layout = wibox.layout.fixed.horizontal,
+        spacing = dpi(15),
+        {
+            widget = widgets.background,
+            forced_width = dpi(20),
+            forced_height = dpi(20),
+            shape = gshape.circle,
+            bg = beautiful.colors.on_background
+        },
+        {
+            widget = widgets.text,
+            size = 15,
+            text = message
+        }
     }
+end
 
+local function new()
     return wibox.widget {
         layout = wibox.layout.overflow.vertical,
         scrollbar_widget = widgets.scrollbar,
         scrollbar_width = dpi(10),
         step = 50,
         spacing = dpi(15),
-        gnome_keyring
+        bullet_point("Secrets are now stored in a secured way. Please install gnome-keyring and re-set your Gitlab API key and Openweather access token")
     }
 end
 
