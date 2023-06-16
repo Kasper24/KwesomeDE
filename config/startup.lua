1adc55150f37193e05f57132ea393c1af76c8cb5
Ofek Itscovits <ofek4430@gmail.com@gmail.com>
2023-06-16 19:08:08 +0300

    Move some stuff out of startup.lua

--- config/startup.lua
+++ config/startup.lua
@@ -5,59 +5,29 @@
 local awful = require("awful")
 local gtimer = require("gears.timer")
 local helpers = require("helpers")
-local string = string
 
-local function run_programs()
-    helpers.run.run_once_ps("polkit-gnome-authentication-agent-1",
-        "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-    helpers.run.run_once_grep("blueman-applet")
-    helpers.run.run_once_grep("parcellite")
-    helpers.run.run_once_grep("kdeconnect-indicator")
-    helpers.run.run_once_grep("mopidy")
-    helpers.run.run_once_grep("bitwarden")
-    awful.spawn("dropbox start -i", false)
-    gtimer.start_new(10, function()
-        helpers.run.run_once_grep("openrgb --server --gui")
-        return false
-    end)
-end
+helpers.run.run_once_grep("blueman-applet")
+helpers.run.run_once_grep("parcellite")
+helpers.run.run_once_grep("kdeconnect-indicator")
+helpers.run.run_once_grep("mopidy")
+helpers.run.run_once_grep("bitwarden")
+helpers.run.run_once_grep("dropbox start -i", "dropbox")
+gtimer.start_new(10, function()
+    helpers.run.run_once_grep("openrgb --server --gui")
+    return false
+end)
 
-local function configure_keyboard()
-    gtimer {
-        autostart = true,
-        call_now = true,
-        single_shot = false,
-        timeout = 600,
-        callback = function()
-            awful.spawn("xset r rate 200 30", false)
+awful.spawn("xset s off", false)
+awful.spawn("xset -dpms", false)
+awful.spawn("xset s noblank", fwalse)
 
-            awful.spawn.with_shell([[
-        xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
-           repeat=no,\
-           type[group1]="ALPHABETIC",\
-           symbols[group1]=[ Caps_Lock, Caps_Lock],\
-           actions[group1]=[ LockMods(modifiers=Lock),\
-           Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
-        };\
-        #' | xkbcomp -w 0 - "$DISPLAY"
-           ]])
-        end
-    }
-end
-
-local function configure_display()
-    awful.spawn("xset s off", false)
-    awful.spawn("xset -dpms", false)
-    awful.spawn("xset s noblank", false)
-
-    gtimer.poller {
-        timeout = 600,
-        callback = function()
-            configure_keyboard()
-        end
-    }
-end
-
-run_programs()
-configure_keyboard()
-configure_display()
+awful.spawn.with_shell([[
+    xkbcomp -xkb "$DISPLAY" - | sed 's#key <CAPS>.*#key <CAPS> {\
+       repeat=no,\
+       type[group1]="ALPHABETIC",\
+       symbols[group1]=[ Caps_Lock, Caps_Lock],\
+       actions[group1]=[ LockMods(modifiers=Lock),\
+       Private(type=3,data[0]=1,data[1]=3,data[2]=3)]\
+    };\
+    #' | xkbcomp -w 0 - "$DISPLAY"
+]])
