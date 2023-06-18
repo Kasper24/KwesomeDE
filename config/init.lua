804d861061ee5eedd11e0637c18b4b535ddaa0d1
Ofek Itscovits <ofek4430@gmail.com@gmail.com>
2023-06-18 05:44:36 +0300

    No need for this anymore, I manage it at the system level

--- config/init.lua
+++ config/init.lua
@@ -2,11 +2,6 @@
 -- @author https://github.com/Kasper24
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
--- Only runs this on my system, other don't need it
-if os.getenv("USER") == 'kasper' and DEBUG ~= true then
-    require(... .. ".startup")
-end
-
 require(... .. ".apps")
 require(... .. ".layouts")
 require(... .. ".tags")
