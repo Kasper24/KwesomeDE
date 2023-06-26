bce6273ee28e82cec1dbb1f409140311a144bc87
Ofek Itscovits <ofek4430@gmail.com@gmail.com>
2023-06-26 17:22:28 +0300

    Update icon_theme helper to always use the candy-icons theme

--- helpers/icon_theme.lua
+++ helpers/icon_theme.lua
@@ -3,42 +3,21 @@
 -- @copyright 2021-2022 Kasper24
 -------------------------------------------
 local lgi = require("lgi")
-local Gio = lgi.Gio
-local DesktopAppInfo = Gio.DesktopAppInfo
-local AppInfo = Gio.DesktopAppInfo
 local Gtk = lgi.require("Gtk", "3.0")
 local beautiful = require("beautiful")
-local string = string
+local filesystem = require("external.filesystem")
 local ipairs = ipairs
 
 local _icon_theme = {}
 
 local ICON_SIZE = 48
 local GTK_THEME = Gtk.IconTheme.get_default()
+GTK_THEME:set_custom_theme("candy-icons")
+GTK_THEME:set_search_path({filesystem.filesystem.get_awesome_config_dir("assets")})
 
 local icons = {}
 
-function _icon_theme.has_icon(icon_name, icon_theme, icon_size)
-    if icon_theme then
-        GTK_THEME = Gtk.IconTheme.new()
-        Gtk.IconTheme.set_custom_theme(GTK_THEME, icon_theme);
-    end
-    if icon_size then
-        ICON_SIZE = icon_size
-    end
-
-    return GTK_THEME:has_icon(icon_name)
-end
-
-function _icon_theme.choose_icon(icons_names, icon_theme, icon_size)
-    if icon_theme then
-        GTK_THEME = Gtk.IconTheme.new()
-        Gtk.IconTheme.set_custom_theme(GTK_THEME, icon_theme);
-    end
-    if icon_size then
-        ICON_SIZE = icon_size
-    end
-
+function _icon_theme.choose_icon(icons_names)
     local icon_info = GTK_THEME:choose_icon(icons_names, ICON_SIZE, 0);
     if icon_info then
         local icon_path = icon_info:get_filename()
@@ -50,17 +29,9 @@ function _icon_theme.choose_icon(icons_names, icon_theme, icon_size)
     return nil
 end
 
-function _icon_theme.get_gicon_path(gicon, icon_theme, icon_size)
+function _icon_theme.get_gicon_path(gicon)
     if gicon == nil then
-        return ""
-    end
-
-    if icon_theme then
-        GTK_THEME = Gtk.IconTheme.new()
-        Gtk.IconTheme.set_custom_theme(GTK_THEME, icon_theme);
-    end
-    if icon_size then
-        ICON_SIZE = icon_size
+        return nil
     end
 
     local icon_info = GTK_THEME:lookup_by_gicon(gicon, ICON_SIZE, 0);
@@ -74,17 +45,9 @@ function _icon_theme.get_gicon_path(gicon, icon_theme, icon_size)
     return nil
 end
 
-function _icon_theme.get_icon_path(icon_name, icon_theme, icon_size)
+function _icon_theme.get_icon_path(icon_name)
     if icons[icon_name] then
         return icons[icon_name]
-    end
-
-    if icon_theme then
-        GTK_THEME = Gtk.IconTheme.new()
-        Gtk.IconTheme.set_custom_theme(GTK_THEME, icon_theme);
-    end
-    if icon_size then
-        ICON_SIZE = icon_size
     end
 
     local icon_info = GTK_THEME:lookup_icon(icon_name, ICON_SIZE, 0)
@@ -99,14 +62,14 @@ function _icon_theme.get_icon_path(icon_name, icon_theme, icon_size)
     return nil
 end
 
-function _icon_theme.get_app_icon_path(icon_names, icon_theme, icon_size)
+function _icon_theme.get_app_icon_path(icon_names)
     if type(icon_names) == "table" then
-        return _icon_theme.choose_icon(icon_names, icon_theme, icon_size) or
-                _icon_theme.get_icon_path("application-x-ktheme", icon_theme, icon_size)
+        return _icon_theme.choose_icon(icon_names) or
+                _icon_theme.get_icon_path("application-x-ktheme")
     end
 
-    return _icon_theme.get_icon_path(icon_names, icon_theme, icon_size) or
-            _icon_theme.get_icon_path("application-x-ktheme", icon_theme, icon_size)
+    return _icon_theme.get_icon_path(icon_names) or
+            _icon_theme.get_icon_path("application-x-ktheme")
 end
 
 function _icon_theme:get_app_font_icon(...)
