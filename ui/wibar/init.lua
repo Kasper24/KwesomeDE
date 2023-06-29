3b6015f1e8fb85a37cf0760db18c78765101eb82
Ofek Itscovits <ofek4430@gmail.com@gmail.com>
2023-06-29 21:57:20 +0300

    Rename top to horizontal and left to vertical

--- ui/wibar/init.lua
+++ ui/wibar/init.lua
@@ -1,9 +1,9 @@
 local ui_daemon = require("daemons.system.ui")
 if ui_daemon:get_bars_layout() == "vertical_horizontal" then
-    require(... .. ".left")
-    require(... .. ".top")
+    require(... .. ".vertical")
+    require(... .. ".horizontal")
 elseif ui_daemon:get_bars_layout() == "vertical" then
-    require(... .. ".left")
+    require(... .. ".vertical")
 elseif ui_daemon:get_bars_layout() == "horizontal" then
-    require(... .. ".top")
+    require(... .. ".horizontal")
 end
